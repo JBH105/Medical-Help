3be85699a7daff3b08c56305e4fe0f5b17326d56
JAYDEEP <jaydipbhayani105@gmail.com>
2020-09-07 16:19:59 +0530

    Medical Help

--- app/src/main/java/com/example/email/selecte_user_type.java
+++ app/src/main/java/com/example/email/selecte_user_type.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class selecte_user_type extends AppCompatActivity {
     Button docter,laboratory;
 
@@ -31,5 +33,14 @@ public class selecte_user_type extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),laboratory_register.class));
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+            Intent i = new Intent(getApplicationContext(),register.class);
+            startActivity(i);
+        }
     }
 }
